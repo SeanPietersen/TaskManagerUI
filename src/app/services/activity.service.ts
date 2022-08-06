f18cead72d399423f5ef20f1f2ec2b9d354760d2
Sean Pietersen <seanpietersen7@gmail.com>
2022-08-06 15:16:24 +0200

    Feat: added activity service to add and get the activity

--- src/app/services/activity.service.ts
+++ src/app/services/activity.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Activity } from '../Models/activity';
+import { HttpService } from './http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  uploadActivities(file: File): Observable<Activity>
+  {
+    return this.httpService.post<Activity>('upload', file);
+  }
+
+  getActivities(): Observable<Activity[]>
+  {
+    return this.httpService.get<Activity[]>('retrieve');
+  }
 }
